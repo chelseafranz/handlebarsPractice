2c887117de987a6e94c1c5ea88587031f0ea35e0
Chelsea Franz <chelseafranz3@gmail.com>
2014-10-28 09:31:11 -0400

    updated

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,14 @@
-var library = {
-  books : [
-    { book: 'Moby Dick', author: 'Herman Melville' },
-    { book: 'Great Expectations', author: 'Charles Dickens'},
-    { book: 'Where the Red Fern Grows', author: 'Wilson Rawls'}
-  ]
-};
 
-// var source=$('#books').html();
-// var template= Handlebars.compile(source);
-// $'.books').html(template(data));
-
-//grabbing the template string
-// var template= $('#book').html();
-//create handlebars function
-// var rendered = Handlebars.templates['books'];
-// $('#books').append(rendered(library));
-
-//name of template is name of the tamplate file
-
-//takes what is inside of our template, and creates a function that we can pass an object into
-
-//append to my books elemet
-// 
 var music2 = {
 songs: [
 {song: 'Take Me to Church', artist: 'Hozier'},
 {song: 'Thnks fr th Mmrs', artist: "Fall Out Boy"},
-{song: 'Back in Black', artist: 'ACDC'}
+{song: 'Rude', artist: 'Who The Hell Cares?'},
+{song: '1000 Miles', artist: 'Vanessa Carlton'},
+{song: 'Shake It', artist: 'Taylor Swift'},
+{song: 'Jurassic Park Theme Song', artist: 'Melodica Cover'},
+
+
 ]
 };
 
